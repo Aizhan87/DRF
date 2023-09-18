674fdbf250f2cae2a8c1afef7c3af11321837767
Aizhan87 <moonsoul87@hotmail.com>
2023-09-18 15:32:42 -0600

    added user to managers group

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -37,10 +37,20 @@ class MenuItemView(generics.RetrieveUpdateDestroyAPIView):
             permission_classes = [IsAuthenticated, IsAdminUser]
         return[permission() for permission in permission_classes]
     
+    
 class ManagerListView(generics.ListCreateAPIView):
-    queryset = User.objects.filter(groups__name='Managers')
+    queryset = User.objects.filter(groups__name='Manager')
     serializer_class = ManagerListSerializer
     permission_classes = [IsAuthenticated, IsAdminUser]
+
+    def post(self, request, *args, **kwargs):
+        username = request.data['username']
+        if username:
+            user = get_object_or_404(User, username=username)
+            managers = Group.objects.get(name='Manager')
+            managers.user_set.add(user)
+            return JsonResponse(status=201, data={'message':'User added to Managers group'}) 
+
     
     
 
